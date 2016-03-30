f39adba47230d2a84e1dcfa04175458c4936e998
Adam Prescott <adam@aprescott.com>
2016-03-29 23:42:36 -0400

    Bump to version 0.0.6.

--- build/tenuki.js
+++ build/tenuki.js
@@ -1,5 +1,5 @@
 /*!
- * tenuki.js v0.0.5 (https://github.com/aprescott/tenuki.js)
+ * tenuki.js v0.0.6 (https://github.com/aprescott/tenuki.js)
  * Copyright © 2016 Adam Prescott.
  * Licensed under the MIT license.
  */
