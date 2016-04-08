11f76a964b677ab1f4653444f298884dcec6b5e9
Adam Prescott <adam@aprescott.com>
2016-04-07 20:22:44 -0400

    Update pages.

---
+++ examples/example-controls.js
@@ -0,0 +1,82 @@
+// An example setup showing how buttons could be set to board/game functionality.
+ExampleGameControls = function(element, game) {
+  this.element = element;
+  this.game = game;
+  this.textInfo = element.querySelector(".text-info p");
+  this.gameInfo = element.querySelector(".game-info p");
+
+  this.setText = function(str) {
+    this.textInfo.innerText = str;
+  };
+
+  this.updateStats = function() {
+    var newGameInfo = "";
+    newGameInfo += "Black stones captured: " + this.game.captures["black"];
+    newGameInfo += "\n\n";
+    newGameInfo +=  "White stones captured: " + this.game.captures["white"];
+    newGameInfo += "\n\n";
+
+    newGameInfo += "Move " + this.game.moves.length;
+    newGameInfo += "\n\n";
+
+    var currentMove = this.game.currentMove();
+
+    if (currentMove) {
+      var player = currentMove.color[0].toUpperCase() + currentMove.color.substr(1);
+
+      if (currentMove.pass) {
+        newGameInfo += player + " passed."
+      }
+    }
+
+    this.gameInfo.innerText = newGameInfo;
+
+    if (typeof currentMove != "undefined" && currentMove.pass) {
+      var str = "";
+
+      if (this.game.isOver()) {
+        str += "Game over.";
+        str += "\n\n"
+        str += "Territory scoring: Black has " + this.game.territoryScore().black;
+        str += "\n\n";
+        str += "Territory scoring: White has " + this.game.territoryScore().white;
+        str += "\n\n"
+        str += "Area scoring: Black has " + this.game.areaScore().black;
+        str += "\n\n"
+        str += "Area scoring: White has " + this.game.areaScore().white;
+      }
+
+      this.setText(str)
+    } else {
+      this.setText("");
+    }
+  };
+
+  this.setup = function() {
+    var controls = this;
+
+    var passButton = document.querySelector(".pass");
+    var undoButton = document.querySelector(".undo");
+    var texturedButton = document.querySelector(".textured");
+
+    passButton.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      var player = controls.game.currentPlayer;
+      controls.game.pass();
+      controls.updateStats();
+    });
+
+    undoButton.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      controls.game.undo();
+    });
+
+    texturedButton.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      controls.game.renderer.boardElement.classList.toggle("tenuki-board-textured");
+    });
+  }
+};
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-git checkout master -- examples/*.html
-git checkout master -- build
+git checkout master -- examples/
+git checkout master -- build/
 git checkout master -- test.html
