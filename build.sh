0a645364dd54ba83654c55851f69286ed596f6dc
Adam Prescott <adam@aprescott.com>
2016-04-10 21:17:57 -0400

    Mangle the minified JavaScript

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export PATH="./node_modules/.bin:$PATH"
 
 cat copyright_header.txt <(browserify index.js --standalone tenuki -t [ babelify --presets [ es2015 ] ]) > build/tenuki.js
-cat copyright_header.txt <(uglifyjs build/tenuki.js) > build/tenuki.min.js
+cat copyright_header.txt <(uglifyjs build/tenuki.js --mangle) > build/tenuki.min.js
 cat copyright_header.txt css/*.css > build/tenuki.css
 cat copyright_header.txt <(cat css/*.css | cleancss) > build/tenuki.min.css
