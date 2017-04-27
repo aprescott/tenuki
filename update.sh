93b8878b8ce6ce5584a9273018c0ed45f5c83ab4
Adam Prescott <adam@aprescott.com>
2017-04-26 20:56:08 -0400

    Amend update.sh script

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
-git checkout master -- examples/
-git checkout master -- build/
-git checkout master -- test.html
+set -euo pipefail
+
+git checkout master
+make clean && make && make test
+rm -rf tmp/gh-pages
+rsync -avh build examples test.html tmp/gh-pages/
+git checkout gh-pages
+
+mv tmp/gh-pages/* ./
